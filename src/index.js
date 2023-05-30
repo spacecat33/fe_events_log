8418547bd6fb2b4bc779462d0840cb9eccb8b44c
Janan Akkad <jananakkad@hotmail.co.uk>
2023-05-30 16:17:39 +0100

    added folders to src and updated index.js imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// import './index.css';
+import { provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
