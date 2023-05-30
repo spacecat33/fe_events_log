0c119cb4a676c5c3c743c8c04906d6e5c52f6725
Janan Akkad <jananakkad@hotmail.co.uk>
2023-05-30 17:54:00 +0100

    updated app.js with router, switch and others from react-router-dom:

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
+import Home from './components/Home';
+import Error from './components/Error';
 
 class App extends Component {
   render() {
@@ -10,8 +12,10 @@ class App extends Component {
       <Router> 
          <Nav>
           <Switch>
+            <Route exact path="/" component={ Home } />
+            <Route component={ Error} />
           </Switch>
-          </Nav>
+        </Nav>
           <Footer />
       </Router>
     );
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Home extends Component {
   render() {
     return (
-      <div className="Home">
+      <div>
         Welcome to my app showing all events I've been to this year!
         
         This is the Home Page. You've landed in the right place!
