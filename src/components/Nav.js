21ff979d9c4aea4d391f6eea56409be5c0e31f6e
Janan Akkad <jananakkad@hotmail.co.uk>
2023-05-30 18:36:11 +0100

    added events link to Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -9,6 +9,7 @@ class Nav extends Component {
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
+          <li><Link to="/events">Events</Link></li>
         </ul>
       </div>
     );
