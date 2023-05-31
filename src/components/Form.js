6a83c5cdd42ee3501e1b6eb350deba0eb62c45ed
Janan Akkad <jananakkad@hotmail.co.uk>
2023-05-31 11:09:27 +0100

    building form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addEvent } from '../actions/eventsIndex';
 
 class Form extends Component {
   render() {
     return (
-      <div className="Form">
-        Form page
+      <div>
+        <h3>Create diary entry of an event</h3>
+        <form onSubmit={ this.handleSubmit }>
+        <div>
+            <label htmlFor="title">Title </label>
+            <input type="text" id="title" name="title" value={ this.state.title } onChange={ this.handleChange } />
+          </div>
+          <br />
+          <div>
+            <p><label htmlFor="month">Date</label></p>
+            <textarea id="month" name="month" value={ this.state.month } onChange={ this.handleChange }>
+          </div>
+          <br />
+          <div>
+            <p><label htmlFor="country">Country</label></p>
+            <textarea id="country" name="country" value={ this.state.country } onChange={ this.handleChange }>
+          </div>
+          <br />
+          <div>
+            <p><label htmlFor="diary">Diary details</label></p>
+            <textarea id="diary" name="diary" value={ this.state.diary } onChange={ this.handleChange }></textarea>
+          </div>
+          <br />
+          <input type="submit" value="Create Event" />
+        </form>
       </div>
     );
   }
