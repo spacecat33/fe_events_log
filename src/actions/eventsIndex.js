c22bd73e326cda3c1215efd6c0c6b3d988964018
Janan Akkad <jananakkad@hotmail.co.uk>
2023-05-31 11:03:05 +0100

    updated redux actions eventsIndex with additional fetch and dispatch code

--- src/actions/eventsIndex.js
+++ src/actions/eventsIndex.js
@@ -1,8 +1,26 @@
 export const getEvents = () => {
     return dispatch => {
         dispatch({ type: "LOADING"})
-        fetch("http://localhost:3001/events")
+        fetch('http://localhost:3001/events')
         .then(resp => resp.json())
         .then(events => dispatch({ type: "SET_EVENTS", events }))
+    }
+
+    export const addEvent = (event, history) => {
+        return dispatch => {
+            fetch('http://localhost:3001/events', {
+                method: "POST",
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ event })
+            })
+            .then(resp => resp.json())
+            .then(event => {
+                dispatch({ type: "ADD EVENT", event})
+                history.push("/events")
+            })
+        }
     }
 }
