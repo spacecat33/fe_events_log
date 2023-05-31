282aa1e9f8939b8f9ca0a2992b73ba57e29099f5
Janan Akkad <jananakkad@hotmail.co.uk>
2023-05-31 10:52:56 +0100

    updated eventsIndex.js to define events variable and map over each event to display on webpage

--- src/components/eventsIndex.js
+++ src/components/eventsIndex.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Event from './Event';
 
 class eventsIndex extends Component {
   render() {
+    const events = this.props.events.map( (blog, i) => <Blog key={i} title={ event.title } date={ event.month } country={ event.country } diary={ event.diary } />)
+    
     return (
-      <div className="eventsIndex">
+      <div>
         List of Events
+        { events }
       </div>
     );
   }
